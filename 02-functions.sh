1c42e593c0606e6e9530c915e06f3ac400a82a55
MannuA19 <mannmeet.ahir@gmail.com>
2025-06-19 08:09:13 +0530

    leaning functions

---
+++ 02-functions.sh
@@ -0,0 +1,27 @@
+#function is the name given to a set of commands.
+# syntax
+
+# funtion abc() {
+# commands
+# commands
+# }
+
+# OR
+
+# abc() {
+# ls
+# touch f1 f2 f3
+# ls
+# }
+
+# how should I call a function in a script file - Simply calling function like a command
+
+# abc
+
+SAMPLE() {
+  echo "This is a Sample Function"
+}
+
+SAMPLE
+SAMPLE
+
